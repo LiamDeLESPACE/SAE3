2f1e73e3d759e2178e62c6b79a9f1f49bef2e7e8
LiamDeLESPACE <liam.medkour@etu.univ-smb.fr>
2023-11-20 13:16:01 +0000

    commitmalveillant

---
+++ app/Http/Controllers/AnnonceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Annonce;
+
+use Illuminate\Http\Request;
+
+class AnnonceController extends Controller
+{
+    public function showAnnonce(Request $request, $id){
+        $annonce = Annonce::findOrFail($id);
+        return view("recherche_annonce",["annonce"=>$annonce]);
+    }
+}
---
+++ resources/views/villes.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chouminage</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <form>
+    <input list="html_elements" name="web_language">
+        <datalist id="html_elements">
+            @foreach ($villes as $ville)
+                <option value="{{ $ville->nomville }}"></option>
+            @endforeach
+        </datalist>
+    </form>
+</body>
+</html>
